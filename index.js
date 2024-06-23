d28440e1fafa92254593ba6bfa4d7f8bebccdee1
Shreya Trivedi <shreyatrivedicg@gmail.com>
2024-06-23 13:50:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoList from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
